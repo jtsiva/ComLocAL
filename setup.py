65de9e57fba2b3443fb0c96fd25881cff6f19759
Josh Siva <jtsiva@gmail.com>
2017-10-12 17:21:41 -0400

    adjusted structure for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='comlocal',
+      version='0.1',
+      description='Communication and Localization Abstraction Layer',
+      url='https://github.com/jtsiva/ComLocAL',
+      author='Josh Siva',
+      author_email='jtsiva@gmail.com',
+      license='MIT',
+      packages=['comlocal'],
+      zip_safe=False)
