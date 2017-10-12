64ed2b81f369b2118e4efc8a7b84d9e37106ae38
Josh Siva <jtsiva@gmail.com>
2017-10-12 15:45:39 -0400

    adding comlocal files

---
+++ src/ComLocAL/test/test_ComLocAL.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import unittest
+
+class TestComLocAL(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	@unittest.skip("ComLocAL not implemented")
+	def test_comlocal_init(self):
+		pass
+#
+
+if __name__ == '__main__':
+	unittest.main()
