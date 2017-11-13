31043e99bc2e9315ed04004cf3696fcf8b5d04fe
Josh Siva <jtsiva@gmail.com>
2017-11-13 14:54:54 -0500

    added read handler to Com. Not getting broadcasts on school nw

--- comlocal/core/Com.py
+++ comlocal/core/Com.py
@@ -23,8 +23,45 @@ class Com(object):
 		self._messageL.setWrite(self._routeL.write)
 
 		#for allowing non-blocking writes and an async read callback
+		#TODO: decide if we actually *need* buffering
 		self._inQ = Queue.Queue()
 		self._outQ = Queue.Queue()
+
+		self._readHandler = None
+
+		self._readThread = threading.Thread(target=self._procRead)
+		self._writeThread = threading.Thread(target=self._procWrite)
+	#
+
+	def __del__(self):
+		self.stop() #can't guarantee this will be called, but this is here jic
+
+	def start(self):
+		self._threadsRunning = True
+		self._readThread.start()
+		self._writeThread.start()
+
+	def stop(self):
+		self._threadsRunning = False
+		self._readThread.join()
+		self._writeThread.join()
+
+
+	def _procRead(self):
+		"""
+		Call the read handler on each message received
+		"""
+		
+		while self._threadsRunning:
+			#only do things if the read handler is set
+			#otherwise we'll assume you will call read
+			if self._readHandler is not None:
+				for msg in self._messageL.read():
+					self._readHandler(msg)
+
+	def _procWrite(self):
+		while self._threadsRunning:
+			pass
 
 	def _initCommonData(self, commonData):
 		"""
@@ -50,6 +87,11 @@ class Com(object):
 		"""
 		self._commonData = uniqueID #need to sanitize?
 
+	def setReadHandler(self, cb):
+		"""
+		Read handler callback needs to accept the message as an arg
+		"""
+		self._readHandler = cb
 
 
 	def read(self):
--- comlocal/test/ping.py
+++ comlocal/test/ping.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python
 
 from comlocal.core import Com 
+import json
 import sys
+import pdb
+
+def readHandler(msg):
+	print json.dumps(msg, sort_keys=True, indent=4, separators=(',', ': '))
 
 def main():
 	com = Com.Com()
-
-	#set up read callback
-
-	pings = int(sys.argv[1])
-
-	msg = json.loads('{"type":"msg"}')
-	while pings > 0:
-		msg['payload'] =  pings
-		com.write(msg)
-		pings -= 1
-	#
+	
+	com.setReadHandler(readHandler)
+	com.start()
 
 	try:
+
+		#set up read callback
+
+		pings = int(sys.argv[1]) if len(sys.argv) > 1 else 1000
+
+		msg = json.loads('{"type":"msg"}')
+		while pings > 0:
+			msg['payload'] =  pings
+			com.write(msg)
+			pings -= 1
+		#
+
 		while True:
 			pass
 	except KeyboardInterrupt:
-		break
+		pass
+	finally:
+		com.stop()
 
 
 #
