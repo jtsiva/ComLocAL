6832afb9ec6d39f9b8dba9494378439371bffad4
Josh Siva <jtsiva@gmail.com>
2017-11-28 01:07:25 -0500

    building up the bt equivalent of UDP using l2cap. have some of the bare bones set up but not tested

--- comlocal/radio/Bluetooth.py
+++ comlocal/radio/Bluetooth.py
@@ -1,12 +1,70 @@
+from comlocal.util import Properties
+import Radio
+import json
+import bluetooth
 
-
-class Bluetooth (Radio):
+class Bluetooth (Radio.Radio):
 	"""
 	Abstracts all communication over Bluetooth / BLE
 
 	All communication is broadcast
 	"""
 	def __init__ (self):
+		super(Bluetooth, self).__init__(self._setupProperties())
 		self._name = 'BT'
-		pass
+		self._port = 0x2807
+		self._sock=bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+		self._sock.setblocking(False)
+		self._sock.bind(("", self._port))
+		self._sock.listen(1)
+		
+
+	def _setupProperties(self):
+		"""
+		Set up the radio properties we might need
+		"""
+		props = Properties.Properties()
+
+		props.maxPacketLength = 1024
+		props.costPerByte = 1
+
+		return props
+	#
+
+	def read(self):
+		"""
+		Read from radio and return json object
+
+		Non blocking
+		"""
+
+		try:
+			client_sock,address = server_sock.accept()
+			data = client_sock.recv(self.getProperties().maxPacketLength)
+			tmp = json.loads(data)
+			tmp['sentby'] = address[0] #want the address
+		except socket.timeout:
+			data = '{}'
+			tmp = json.loads(data)
+		finally:
+			client_sock.close()
+		
+		return tmp
+	#
+
+	def write(self, data):
+		"""
+		write json object to radio
+		"""
+
+		#discover devices
+		#bluetooth.set_packet_timeout( bdaddr, timeout)
+		#connect and send to everyone!
+		try:
+			res = self._wSock.sendto(json.dumps(data), self._broadcastAddr)
+		except Exception as e:
+			raise e
+		#
+	#
 #
