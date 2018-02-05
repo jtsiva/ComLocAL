9e19123d316e679bfbd8473ea8cac9c5b864f8f1
Josh Siva <jtsiva@gmail.com>
2018-02-05 16:44:13 -0500

    still messing with things...

--- comlocal/test/pingpong.py
+++ comlocal/test/pingpong.py
@@ -5,21 +5,22 @@ import time
 import argparse
 
 last = time.time()
-readyToSend = True
+sender = True
+txAck = True
 
 class myThing(object):
 	def __init__(self):
 		self.read = 0
 		self.cmdRes = 0
 		self.writeRes = 0
-		self.readyToSend = False
 
 	def reader(self, msg):
 		
 		self.read += 1
 		if self.read % 100 == 0:
 			print msg
-		readyToSend = True
+		sender = True
+		
 		
 
 	def result(self, msg):
@@ -27,7 +28,7 @@ class myThing(object):
 		if 'cmd' in msg:
 			self.cmdRes += 1
 		elif 'msg' in msg:
-			last = time.time()
+			txAck = True
 			self.writeRes += 1
 
 def main():
@@ -45,7 +46,7 @@ def main():
 	myCom.setResultCB (thing.result)
 
 	count = int(args.count)
-	readyToSend = args.first
+	sender = args.first
 	dest = int(args.dest)
 
 	msg = {'type':'msg','msg':'hello','dest':dest}
@@ -56,10 +57,11 @@ def main():
 		writes = 0
 
 		while count > writes:
-			if readyToSend or ((time.time() - last) > .5 and args.first):
+			if sender or (not txAck and (time.time() - last) > .5):
 				myCom.comWrite(msg)
+				txAck = False
 				writes += 1
-				readyToSend = False
+				sender = False
 				last = time.time()
 	except KeyboardInterrupt:
 		print ''
